9334bf5482581e1c713c9f64885a45f1ca77e7ce
Aurélien Le Hyaric <aurelien.lehyaric@live.com>
2024-08-13 08:59:20 +0200

    initialisation

---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./HomePage.scss"
+
+function HomePage() {
+   return (
+      <>
+         <header></header>
+         <div className="homemenu"></div>
+         <div className="introduction"></div>
+         <div className="personnaliser"></div>
+         <div className="etapes"></div>
+         <footer></footer>
+      </>
+   )
+}
+
+export default HomePage
