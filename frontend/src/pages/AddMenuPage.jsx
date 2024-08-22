0fc1d8b6d855a400f9c2d2ef53ad7b8065d9ba3c
Aurélien Le Hyaric <aurelien.lehyaric@live.com>
2024-08-22 12:01:58 +0200

    addmenu page

--- frontend/src/pages/AddMenuPage.jsx
+++ frontend/src/pages/AddMenuPage.jsx
@@ -1,11 +1,23 @@
 import React from "react"
 import "./AddMenuPage.scss"
+import Button from "../components/Button/Button"
 
 function AddMenu() {
    return (
-      <div className="menuspage">
-         <div className="first">Je suis dans mes menus !!!!!!!!!!!!!!!</div>
-         <div className="prevsection">Zone de prev !!!!!!!!!!!!!!!</div>
+      <div className="addmenupage">
+         <div className="main">
+            <h1>Créer un menu</h1>
+            <p>Laissez-vous guider étape par étape dans la création de votre menu, vous pourrez le sauvegarder pour reprendre la création plus tard !</p>
+            <div className="create-section">
+               <div className="partone">one</div>
+               <div className="parttwo">two</div>
+               <div className="partthree">three</div>
+            </div>
+            <Button text={"Étape suivante"} variant={"inactive"} />
+         </div>
+         <div className="preview">
+            <div className="previewmenu"></div>
+         </div>
       </div>
    )
 }
