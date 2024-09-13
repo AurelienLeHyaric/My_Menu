da13e735a796e6199ac8dc38c8e5eeeea0bb1b11
Aurélien Le Hyaric <aurelien.lehyaric@live.com>
2024-09-13 09:42:58 +0200

    ajout plat/categ

--- frontend/src/components/AddMenuSteps/StepOne/StepOne.jsx
+++ frontend/src/components/AddMenuSteps/StepOne/StepOne.jsx
@@ -1,38 +1,139 @@
-import React from "react"
+import React, { useState } from "react"
+import "./StepOne.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons"
 import Button from "../../Button/Button"
+import PlatForm from "../../Menu/PlatForm/PlatForm"
+import CategoryForm from "../../Menu/CategoryForm/CategoryForm"
 
-function StepOne({ categories, plats, onAddCategory, onAddPlat, onDeleteCategory, onDeletePlat, onOpenPlatForm }) {
+function StepOne({ categories, plats, setCategories, setPlats }) {
+   const [selectedCategory, setSelectedCategory] = useState(null)
+   const [isCategoryFormOpen, setCategoryFormOpen] = useState(false)
+   const [isPlatFormOpen, setPlatFormOpen] = useState(false)
+   const [isDeleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false)
+   const [categoryToDelete, setCategoryToDelete] = useState(null)
+   const [isDeletePlatConfirmationOpen, setDeletePlatConfirmationOpen] = useState(false)
+   const [platToDelete, setPlatToDelete] = useState({ category: null, name: null })
+
+   // Ouverture des formulaires
+   const handleOpenCategoryForm = () => setCategoryFormOpen(true)
+   const handleOpenPlatForm = (category) => {
+      setSelectedCategory(category)
+      setPlatFormOpen(true)
+   }
+
+   // Ajout d'une catégorie
+   const handleAddCategory = (category) => {
+      setCategories([...categories, category])
+      setCategoryFormOpen(false)
+   }
+
+   // Ajout d'un plat
+   const handleAddPlat = (newPlat) => {
+      setPlats((prevPlats) => ({
+         ...prevPlats,
+         [selectedCategory]: [...(prevPlats[selectedCategory] || []), newPlat],
+      }))
+      setPlatFormOpen(false)
+   }
+
+   // Suppression d'une catégorie
+   const handleOpenDeleteCategoryConfirmation = (category) => {
+      setCategoryToDelete(category)
+      setDeleteConfirmationOpen(true)
+   }
+
+   const handleConfirmDeleteCategory = () => {
+      setCategories(categories.filter((cat) => cat !== categoryToDelete))
+      const updatedPlats = { ...plats }
+      delete updatedPlats[categoryToDelete]
+      setPlats(updatedPlats)
+      setDeleteConfirmationOpen(false)
+      setCategoryToDelete(null)
+   }
+
+   const handleCancelDeleteCategory = () => {
+      setDeleteConfirmationOpen(false)
+      setCategoryToDelete(null)
+   }
+
+   // Suppression d'un plat
+   const handleOpenDeletePlatConfirmation = (category, platName) => {
+      setPlatToDelete({ category, name: platName })
+      setDeletePlatConfirmationOpen(true)
+   }
+
+   const handleConfirmDeletePlat = () => {
+      setPlats((prevPlats) => {
+         const updatedPlats = { ...prevPlats }
+         updatedPlats[platToDelete.category] = updatedPlats[platToDelete.category].filter((p) => p.name !== platToDelete.name)
+         return updatedPlats
+      })
+      setDeletePlatConfirmationOpen(false)
+      setPlatToDelete({ category: null, name: null })
+   }
+
+   const handleCancelDeletePlat = () => {
+      setDeletePlatConfirmationOpen(false)
+      setPlatToDelete({ category: null, name: null })
+   }
+
    return (
       <div className="step-one">
-         <div className="title">
+         <div className="zone-title">
             <p className="number">1</p>
             <h2>Ajoutez vos plats</h2>
          </div>
-         <div id="addbutton">
+         <div className="menu-list">
             {categories.map((category, index) => (
                <div key={index}>
-                  <p>
-                     {category}
+                  <div className="category-item">
+                     <p>{category}</p>
                      <FontAwesomeIcon icon={faPenToSquare} />
-                     <FontAwesomeIcon icon={faTrashCan} onClick={() => onDeleteCategory(category)} />
-                  </p>
+                     <FontAwesomeIcon icon={faTrashCan} onClick={() => handleOpenDeleteCategoryConfirmation(category)} />
+                  </div>
                   {plats[category] && (
-                     <ul>
+                     <ul className="plat-list">
                         {plats[category].map((plat, platIndex) => (
-                           <li key={platIndex}>
-                              {plat}
+                           <li className="plat-item" key={platIndex}>
+                              <span>{plat.name}</span>
                               <FontAwesomeIcon icon={faPenToSquare} />
-                              <FontAwesomeIcon icon={faTrashCan} onClick={() => onDeletePlat(category, plat)} />
+                              <FontAwesomeIcon icon={faTrashCan} onClick={() => handleOpenDeletePlatConfirmation(category, plat.name)} />
                            </li>
                         ))}
                      </ul>
                   )}
-                  <button onClick={() => onOpenPlatForm(category)}>+ Ajouter un plat aux {category}</button>
+                  <button onClick={() => handleOpenPlatForm(category)}>+ Ajouter un plat aux {category}</button>
                </div>
             ))}
-            <Button text={"+ Catégorie"} onClick={onAddCategory} />
+
+            <Button text={"+ Ajouter une catégorie"} onClick={handleOpenCategoryForm} />
+
+            {/* Pop-up de confirmation pour la suppression de catégorie */}
+            {isDeleteConfirmationOpen && (
+               <div className="delete-confirmation">
+                  <p>Êtes-vous sûr de vouloir supprimer la catégorie "{categoryToDelete}" et tous ses plats ?</p>
+                  <button onClick={handleConfirmDeleteCategory}>Confirmer</button>
+                  <button onClick={handleCancelDeleteCategory}>Annuler</button>
+               </div>
+            )}
+
+            {/* Pop-up de confirmation pour la suppression de plat */}
+            {isDeletePlatConfirmationOpen && (
+               <div className="delete-confirmation">
+                  <p>
+                     Êtes-vous sûr de vouloir supprimer le plat "{platToDelete.name}" dans la catégorie "{platToDelete.category}" ?
+                  </p>
+                  <button onClick={handleConfirmDeletePlat}>Confirmer</button>
+                  <button onClick={handleCancelDeletePlat}>Annuler</button>
+               </div>
+            )}
+
+            {/* Formulaire d'ajout de catégorie */}
+            {isCategoryFormOpen && <CategoryForm onClose={() => setCategoryFormOpen(false)} onAddCategory={handleAddCategory} />}
+
+            {/* Formulaire d'ajout de plat */}
+            {isPlatFormOpen && <PlatForm onClose={() => setPlatFormOpen(false)} onAddPlat={handleAddPlat} categoryName={selectedCategory} />}
          </div>
       </div>
    )
--- frontend/src/pages/AddMenuPage.jsx
+++ frontend/src/pages/AddMenuPage.jsx
@@ -4,93 +4,44 @@ import Button from "./../components/Button/Button"
 import StepOne from "./../components/AddMenuSteps/StepOne/StepOne"
 import StepTwo from "./../components/AddMenuSteps/StepTwo/StepTwo"
 import StepThree from "./../components/AddMenuSteps/StepThree/StepThree"
-import PlatForm from "../components/Menu/PlatForm/PlatForm"
-import CategoryForm from "../components/Menu/CategoryForm/CategoryForm"
 
 function AddMenu() {
-   const [currentStep, setCurrentStep] = useState(1) // État pour suivre l'étape actuelle
+   const [currentStep, setCurrentStep] = useState(1)
    const [categories, setCategories] = useState([])
    const [plats, setPlats] = useState({})
-   const [selectedCategory, setSelectedCategory] = useState(null)
-   const [isCategoryFormOpen, setCategoryFormOpen] = useState(false)
-   const [isPlatFormOpen, setPlatFormOpen] = useState(false)
 
    const handleNextStep = () => setCurrentStep((prevStep) => prevStep + 1)
-
-   const handleAddCategory = (category) => {
-      setCategories([...categories, category])
-      setCategoryFormOpen(false)
-   }
-
-   const handleAddPlat = (newPlat) => {
-      setPlats((prevPlats) => ({
-         ...prevPlats,
-         [selectedCategory]: [...(prevPlats[selectedCategory] || []), newPlat],
-      }))
-      setPlatFormOpen(false)
-   }
-
-   const handleDeleteCategory = (category) => {
-      setCategories(categories.filter((cat) => cat !== category))
-      const updatedPlats = { ...plats }
-      delete updatedPlats[category]
-      setPlats(updatedPlats)
-   }
-
-   const handleDeletePlat = (category, plat) => {
-      setPlats((prevPlats) => {
-         const updatedPlats = { ...prevPlats }
-         updatedPlats[category] = updatedPlats[category].filter((p) => p !== plat)
-         return updatedPlats
-      })
-   }
-
-   const handleCustomize = (customization) => {
-      console.log("Customization applied: ", customization)
-   }
-
-   const handleExport = (method) => {
-      console.log(`Exporting via ${method}`)
-   }
+   const handlePrevStep = () => setCurrentStep((prevStep) => prevStep - 1)
 
    return (
       <div className="addmenupage">
          <div className="main">
-            <h1>Créer un menu</h1>
-            {/* Étape 1 : Ajoutez vos plats */}
-            <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
-               {currentStep === 1 && (
-                  <StepOne
-                     categories={categories}
-                     plats={plats}
-                     onAddCategory={() => setCategoryFormOpen(true)}
-                     onAddPlat={handleAddPlat}
-                     onDeleteCategory={handleDeleteCategory}
-                     onDeletePlat={handleDeletePlat}
-                     onOpenPlatForm={(category) => {
-                        setSelectedCategory(category)
-                        setPlatFormOpen(true)
-                     }}
-                  />
-               )}
+            <div className="create-section">
+               <h1>Créer un menu</h1>
+               <p>Laissez-vous guider étape par étape dans la création de votre menu, vous pourrez le sauvegarder pour reprendre la création plus tard !</p>
+
+               {/* Étape 1 : Ajoutez vos plats */}
+               <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
+                  {currentStep === 1 && <StepOne categories={categories} plats={plats} setCategories={setCategories} setPlats={setPlats} />}
+               </div>
+
+               {/* Étape 2 : Personnalisez votre menu */}
+               <div className={`step ${currentStep >= 2 ? "active" : ""}`}>{currentStep === 2 && <StepTwo onCustomize={() => {}} />}</div>
+
+               {/* Étape 3 : Exportez votre menu */}
+               <div className={`step ${currentStep >= 3 ? "active" : ""}`}>{currentStep === 3 && <StepThree onExport={() => {}} />}</div>
+
+               {/* Bouton pour passer à l'étape précédente */}
+               {currentStep > 1 && <Button text={"Étape précédente"} onClick={handlePrevStep} />}
+
+               {/* Bouton pour passer à l'étape suivante */}
+               {currentStep < 3 && <Button text={"Étape suivante"} onClick={handleNextStep} />}
             </div>
-
-            {/* Étape 2 : Personnalisez votre menu */}
-            <div className={`step ${currentStep >= 2 ? "active" : ""}`}>{currentStep === 2 && <StepTwo onCustomize={handleCustomize} />}</div>
-
-            {/* Étape 3 : Exportez votre menu */}
-            <div className={`step ${currentStep >= 3 ? "active" : ""}`}>{currentStep === 3 && <StepThree onExport={handleExport} />}</div>
-
-            {/* Bouton pour passer à l'étape suivante */}
-            {currentStep < 3 && <Button text={"Étape suivante"} onClick={handleNextStep} />}
          </div>
 
          <div className="preview">
             <div className="previewmenu">Prévisualisation du menu ici</div>
          </div>
-
-         {isCategoryFormOpen && <CategoryForm onClose={() => setCategoryFormOpen(false)} onAddCategory={handleAddCategory} />}
-         {isPlatFormOpen && <PlatForm onClose={() => setPlatFormOpen(false)} onAddPlat={handleAddPlat} categoryName={selectedCategory} />}
       </div>
    )
 }
